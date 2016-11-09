956c7c27529ea79b6f1f9e3290ce4117183822d6
Ryohei_Ichii <ryohei.ichii@gmail.com>
2016-11-09 23:31:06 +0900

    FrontVer.2完成

--- app/src/main/java/jp/ac/hokudai/isdl/ryohei_ichii/dynamicsnowroadnavigation/Front.java
+++ app/src/main/java/jp/ac/hokudai/isdl/ryohei_ichii/dynamicsnowroadnavigation/Front.java
@@ -1,8 +1,10 @@
 package jp.ac.hokudai.isdl.ryohei_ichii.dynamicsnowroadnavigation;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Front extends AppCompatActivity {
 
@@ -10,5 +12,39 @@ public class Front extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
+
+        Button btnrun = (Button)this.findViewById(R.id.button_run);
+        Button btnmap = (Button)this.findViewById(R.id.button_map);
+        Button btninfo = (Button)this.findViewById(R.id.button_info);
+        Button btnpref = (Button)this.findViewById(R.id.button_preference);
+
+        btnrun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Front.this,Run.class);
+                startActivity(intent);
+            }
+        });
+        btnmap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Front.this,Map.class);
+                startActivity(intent);
+            }
+        });
+        btninfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Front.this,Info.class);
+                startActivity(intent);
+            }
+        });
+        btnpref.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Front.this,Pref.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/jp/ac/hokudai/isdl/ryohei_ichii/dynamicsnowroadnavigation/Run.java
@@ -0,0 +1,13 @@
+package jp.ac.hokudai.isdl.ryohei_ichii.dynamicsnowroadnavigation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Run extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_run);
+    }
+}
