4925ff1f7b68bab962b64b0c7baaf97b607f7602
Ryohei Ichii <ryohei_ichii@eis.hokudai.ac.jp>
2016-11-21 23:01:31 +0900

    GPS実装（最低限・仮）

--- app/src/main/java/jp/ac/hokudai/isdl/ryohei_ichii/dynamicsnowroadnavigation/Front.java
+++ app/src/main/java/jp/ac/hokudai/isdl/ryohei_ichii/dynamicsnowroadnavigation/Front.java
@@ -1,51 +1,78 @@
 package jp.ac.hokudai.isdl.ryohei_ichii.dynamicsnowroadnavigation;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Front extends AppCompatActivity {
+
+    private final int REQUEST_PERMISSION = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
 
-        ImageButton btnrun = (ImageButton)this.findViewById(R.id.button_run);
-        ImageButton btnmap = (ImageButton)this.findViewById(R.id.button_map);
-        ImageButton btninfo = (ImageButton)this.findViewById(R.id.button_info);
-        ImageButton btnpref = (ImageButton)this.findViewById(R.id.button_preference);
+        //permission関連
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkPermission();
+        }
+
+
+        ImageButton btnrun = (ImageButton) this.findViewById(R.id.button_run);
+        ImageButton btnmap = (ImageButton) this.findViewById(R.id.button_map);
+        ImageButton btninfo = (ImageButton) this.findViewById(R.id.button_info);
+        ImageButton btnpref = (ImageButton) this.findViewById(R.id.button_preference);
 
         btnrun.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Front.this,Run.class);
+                Intent intent = new Intent(Front.this, Run.class);
                 startActivity(intent);
             }
         });
         btnmap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Front.this,Map.class);
+                Intent intent = new Intent(Front.this, Map.class);
                 startActivity(intent);
             }
         });
         btninfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Front.this,Info.class);
+                Intent intent = new Intent(Front.this, Info.class);
                 startActivity(intent);
             }
         });
         btnpref.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Front.this,Pref.class);
+                Intent intent = new Intent(Front.this, Pref.class);
                 startActivity(intent);
             }
         });
     }
+
+    //permission確認
+    public void checkPermission() {
+        // GPS利用の許可がなかった場合に許可を求める
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(Front.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION);
+            } else {
+                Toast toast = Toast.makeText(this, "Runモードの実行には位置情報の利用許可が必要です", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+    }
+
 }
--- app/src/main/java/jp/ac/hokudai/isdl/ryohei_ichii/dynamicsnowroadnavigation/Run.java
+++ app/src/main/java/jp/ac/hokudai/isdl/ryohei_ichii/dynamicsnowroadnavigation/Run.java
@@ -1,31 +1,85 @@
 package jp.ac.hokudai.isdl.ryohei_ichii.dynamicsnowroadnavigation;
 
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
 
-public class Run extends AppCompatActivity {
+import android.widget.Toast;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    // GoogleMapオブジェクトの宣言
-    private GoogleMap googleMap;
+public class Run extends FragmentActivity implements OnMapReadyCallback, LocationListener {
+
+    private GoogleMap mMap;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //permissionチェック
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                Toast toast = Toast.makeText(this, "位置情報の利用許可されていないため、開始できません", Toast.LENGTH_SHORT);
+                toast.show();
+                return;
+            }
+            //GPS情報取得関連
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 100, this);
+        } catch (Exception e){
+            e.printStackTrace();
+            Toast toast = Toast.makeText(this, "例外が発生、位置情報のPermissionを許可していますか？", Toast.LENGTH_SHORT);
+            toast.show();
+            //MainActivityに戻す
+            finish();
+        }
 
-        // MapFragmentオブジェクトを取得
-        MapFragment mapFragment = (MapFragment) getFragmentManager()
-                .findFragmentById(R.id.map);
+        //Map取得関連
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
 
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        ft.add(android.R.id.content, mapFragment);
-        ft.commit();
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(43, 141);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng currentLoc = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        
     }
 }
