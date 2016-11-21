b5ea4c27426177281afcc08d99c9aaf9d83d5509
Ryohei Ichii <ryohei_ichii@eis.hokudai.ac.jp>
2016-11-21 19:51:37 +0900

    GoogleMap追加

--- app/src/main/java/jp/ac/hokudai/isdl/ryohei_ichii/dynamicsnowroadnavigation/Run.java
+++ app/src/main/java/jp/ac/hokudai/isdl/ryohei_ichii/dynamicsnowroadnavigation/Run.java
@@ -1,13 +1,31 @@
 package jp.ac.hokudai.isdl.ryohei_ichii.dynamicsnowroadnavigation;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class Run extends AppCompatActivity {
+
+    // GoogleMapオブジェクトの宣言
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_run);
+
+        // MapFragmentオブジェクトを取得
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(android.R.id.content, mapFragment);
+        ft.commit();
+
+
     }
 }
